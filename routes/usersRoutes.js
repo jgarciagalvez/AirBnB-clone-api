3737e18e9587ebad1652b077f3e25786aa64599f
I-bangash <irtezabangash3d@gmail.com>
2024-02-15 12:20:50 +0700

    comments: added comments to userRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 const router = Router()
 
+// Fetch all users
 router.get('/users', (req, res) => {
   res.json([
     { id: 1, firstName: 'Alice' },
@@ -9,6 +10,7 @@ router.get('/users', (req, res) => {
   ])
 })
 
+// Fetch user by userID
 router.get('/users/1', (req, res) => {
   res.json({ id: 1, firstName: 'Alice' })
 })
