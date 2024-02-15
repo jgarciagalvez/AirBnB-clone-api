dd455cbdab1e9941d6b86fd99cf911e8aa066c4d
I-bangash <irtezabangash3d@gmail.com>
2024-02-15 12:19:50 +0700

    changed .send to .json

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,14 +3,14 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/users', (req, res) => {
-  res.send([
+  res.json([
     { id: 1, firstName: 'Alice' },
     { id: 2, firstName: 'Bob' }
   ])
 })
 
 router.get('/users/1', (req, res) => {
-  res.send({ id: 1, firstName: 'Alice' })
+  res.json({ id: 1, firstName: 'Alice' })
 })
 
 export default router
