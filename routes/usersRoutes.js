f355a297db982c2bcad207463f781181a6f47eee
I-bangash <irtezabangash3d@gmail.com>
2024-02-16 12:39:34 +0700

    usersRoutes: changed to db queries

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,18 +1,34 @@
 import { Router } from 'express'
+import db from '../db.js'
 
 const router = Router()
 
 // Fetch all users
-router.get('/users', (req, res) => {
-  res.json([
-    { id: 1, firstName: 'Alice' },
-    { id: 2, firstName: 'Bob' }
-  ])
+// router.get('/users', (req, res) => {
+//   res.json([
+//     { id: 1, firstName: 'Alice' },
+//     { id: 2, firstName: 'Bob' }
+//   ])
+// })
+
+// Fetch all users
+router.get('/users', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM users')
+    res.json(rows)
+  } catch (err) {
+    res.json(err)
+  }
 })
 
 // Fetch user by userID
-router.get('/users/1', (req, res) => {
-  res.json({ id: 1, firstName: 'Alice' })
+router.get('/users/1', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM users WHERE user_id = 1')
+    res.json(rows[0])
+  } catch (err) {
+    res.json(err)
+  }
 })
 
 export default router
