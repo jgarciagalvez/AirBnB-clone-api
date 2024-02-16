8c9813e12c38f2747c4c10de91be64a5522409b4
I-bangash <irtezabangash3d@gmail.com>
2024-02-16 10:13:26 +0700

    routes: added photos in photosRoutes.js and index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express'
 import userRouter from './routes/usersRoutes.js'
 import housesRouter from './routes/housesRoutes.js'
 import reviewsRouter from './routes/reviewsRoutes.js'
+import bookingsRouter from './routes/bookingsRoutes.js'
+import photosRouter from './routes/photosRoutes.js'
 
 const app = express()
 
@@ -11,6 +13,7 @@ app.use(housesRouter)
 app.use(reviewsRouter)
 
 app.use(bookingsRouter)
+app.use(photosRouter)
 
 app.listen(4100, () => {
   console.log('Airbnb API ready on localhost:4100')
