58816c3d7ff8f2efd7fc270159c8e133879e2856
I-bangash <irtezabangash3d@gmail.com>
2024-02-16 14:08:18 +0700

    updated photos queries

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -1,37 +1,28 @@
 import { Router } from 'express'
+import db from '../db.js'
 
 const router = Router()
 
-// dummy data
-const photos = [
-  {
-    id: 1,
-    user_id: 1,
-    house_id: 1,
-    url: 'https://randomuser.me/api/portraits/men/90.jpg'
-  },
-  {
-    id: 2,
-    user_id: 2,
-    house_id: 2,
-    url: 'https://randomuser.me/api/portraits/men/90.jpg'
-  },
-  {
-    id: 3,
-    user_id: 3,
-    house_id: 3,
-    url: 'https://randomuser.me/api/portraits/men/90.jpg'
-  }
-]
-
 // Fetch all photos
-router.get('/photos', (req, res) => {
-  res.json(photos)
+router.get('/photos', async (req, res) => {
+  try {
+    const { rows } = await db.query('SELECT * FROM house_pics')
+    res.json(rows)
+  } catch (err) {
+    res.json(err)
+  }
 })
 
 // Fetch photos with index of 1
-router.get('/photos/1', (req, res) => {
-  res.json(photos[1])
+router.get('/photos/2', async (req, res) => {
+  try {
+    const { rows } = await db.query(
+      'SELECT * FROM house_pics WHERE house_pic_id = 2'
+    )
+    res.json(rows[0])
+  } catch (err) {
+    res.json(err)
+  }
 })
 
 export default router
