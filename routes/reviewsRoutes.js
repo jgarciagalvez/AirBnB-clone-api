c6b08f4d1f0146da38b2d5b4ede7e205144e7397
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-20 13:36:20 +0700

    POST: Insert new review in DB

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -5,6 +5,36 @@ import db from '../db.js'
 // Create router
 const router = Router()
 
+// Define a POST route for reviews
+router.post('/reviews', async (req, res) => {
+  // Deconstract request body
+  const { house_id, reviewer_id, review_date, rating, review_text } = req.body
+  // Define finalQuery to INSERT newReview into DB
+  let finalQuery = `
+    INSERT INTO reviews (
+        house_id,
+        reviewer_id,
+        review_date,
+        rating,
+        review_text
+    )
+    VALUES(
+      '${house_id}',
+      '${reviewer_id}',
+      '${review_date}',
+      '${rating}',
+      '${review_text}'
+    )
+    RETURNING *`
+  // Query DB
+  try {
+    const newReview = await db.query(finalQuery)
+    res.json(newReview)
+  } catch (err) {
+    res.json({ error: err.message })
+  }
+})
+
 // Define a GET route for fetching all reviews
 router.get('/reviews', async (req, res) => {
   try {
