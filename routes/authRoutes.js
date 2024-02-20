625dd6e198608bc1d4eac3b01c93d168844f0dc3
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-20 13:14:10 +0700

    Add comments and standarise code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,24 +2,27 @@
 import { Router } from 'express'
 import db from '../db.js'
 
-// Get user data
+// Start app
 const router = Router()
 
+// Dummy data for GET calls
 const userdata = [
   { user_id: 1, username: 'john@mail.com', password: 1234 },
   { user_id: 2, username: 'mary@mail.com', password: 56789 }
 ]
 
-// Define a Get route for signup
+// POST route for signup
 router.post('/signup', async (req, res) => {
+  // Deconstruct body request
   const { first_name, last_name, profile_pic, email, password } = req.body
 
+  // Define finalQuery to fetch data from DB
   const finalQuery = `INSERT INTO users (first_name, last_name, profile_pic, email, password)
   VALUES ('${first_name}', '${last_name}', '${profile_pic}', '${email}', '${password}')
   RETURNING *`
 
   try {
-    // console.log('Final users post query: ', finalQuery)
+    // Fetch data from DB
     const { rows } = await db.query(finalQuery)
 
     res.json(rows)
@@ -28,17 +31,21 @@ router.post('/signup', async (req, res) => {
   }
 })
 
-// Define a Get route for login
+// Define a POST route for login
 router.post('/login', async (req, res) => {
-  let query = `
+  // Deconstruct body request
+  const { email, password } = req.body
+  // Define finalQuery to fetch data from DB
+  const finalQuery = `
     SELECT * FROM users 
     WHERE
-    email = '${req.body.email}'
-    AND password = '${req.body.password}'
+    email = '${email}'
+    AND password = '${password}'
     `
-  console.log(query)
   try {
-    const { rows } = await db.query(query)
+    // Fetch data from DB
+    const { rows } = await db.query(finalQuery)
+    // Return an error if no user is found
     if (!rows.length) {
       throw new Error('Incorrect Login Credentials')
     }
