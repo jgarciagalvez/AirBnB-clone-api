5244e1042332571204239f4b0de760d3798e6dbd
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-03-29 19:54:09 +0700

    Add Author data to reviews response

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -1,16 +1,30 @@
 // Import files
 import { Router } from 'express'
 import db from '../db.js'
+import jwt from 'jsonwebtoken'
+const jwtSecret = process.env.JWSECRET
 
 // Create router
 const router = Router()
 
 // Define a POST route for reviews
 router.post('/reviews', async (req, res) => {
-  // Deconstract request body
-  const { house_id, reviewer_id, review_date, rating, review_text } = req.body
-  // Define finalQuery to INSERT newReview into DB
-  let finalQuery = `
+  try {
+    // Validate Token
+    const decodedToken = jwt.verify(req.cookies.jwt, jwtSecret)
+    if (!decodedToken || !decodedToken.user_id || !decodedToken.email) {
+      throw new Error('Invalid authentication token')
+    }
+
+    // Deconstract request body
+    const { house_id, rating, review_text } = req.body
+
+    // Get current date
+    let date = new Date()
+    date = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate()
+
+    // Define finalQuery to INSERT newReview into DB
+    let finalQuery = `
     INSERT INTO reviews (
         house_id,
         reviewer_id,
@@ -20,15 +34,30 @@ router.post('/reviews', async (req, res) => {
     )
     VALUES(
       '${house_id}',
-      '${reviewer_id}',
-      '${review_date}',
+      '${decodedToken.user_id}',
+      '${date}',
       '${rating}',
       '${review_text}'
     )
     RETURNING *`
-  // Query DB
-  try {
-    const newReview = await db.query(finalQuery)
+
+    // Insert Review
+    let { rows } = await db.query(finalQuery)
+
+    // Get author fields
+    let { rows: userRows } = await db.query(`
+      SELECT users.first_name, users.last_name, users.profile_pic FROM users
+      WHERE user_id = ${decodedToken.user_id}
+    `)
+    let newReview = rows[0]
+    newReview.author = userRows[0]
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric'
+    })
+    const formatted = formatter.format(new Date(newReview.review_date))
+    newReview.review_date = formatted
     res.json(newReview)
   } catch (err) {
     res.json({ error: err.message })
@@ -37,19 +66,39 @@ router.post('/reviews', async (req, res) => {
 
 // Define a GET route for fetching all reviews
 router.get('/reviews', async (req, res) => {
-  const { house_id } = req.query
-
-  const finalQuery = !house_id
-    ? `SELECT * FROM reviews ORDER BY review_date DESC`
-    : `SELECT * FROM reviews WHERE house_id = ${house_id} ORDER BY review_date DESC`
-
-  // console.log('Final query reviews GET: ', finalQuery)
   try {
-    const { rows } = await db.query(finalQuery)
-    if (!rows.length) {
-      throw new Error('No reviews found for this house ID')
+    if (!req.query.house_id) {
+      throw new Error('house_id is required')
     }
-    res.json(rows)
+    let sqlquery = `
+      SELECT reviews.*, users.first_name, users.last_name, users.profile_pic FROM reviews
+      LEFT JOIN users ON users.user_id = reviews.reviewer_id
+      WHERE house_id = ${req.query.house_id}
+      ORDER BY review_date DESC
+    `
+    let { rows } = await db.query(sqlquery)
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric'
+    })
+
+    // Move user data to sub-object author
+    let reviews = rows.map((r) => {
+      r.author = {
+        first_name: r.first_name,
+        last_name: r.last_name,
+        profile_pic: r.profile_pic
+      }
+      r.date = formatter.format(new Date(r.date))
+      delete r.first_name
+      delete r.last_name
+      delete r.profile_pic
+      return r
+    })
+
+    // API Response
+    res.json(reviews)
   } catch (err) {
     res.json({ error: err.message })
   }
